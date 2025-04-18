33c729cbbd80f1720cbdcc6a3fc8e2332e376c35
VazgenAyv <a.wasgen@gmail.com>
2025-04-18 13:41:20 +0400

    finalization of task status check middleware

--- handlers/task.go
+++ handlers/task.go
@@ -159,7 +159,7 @@ func UpdateTask(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]string{"message": "Task Updated"})
 }
